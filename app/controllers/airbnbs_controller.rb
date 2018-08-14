d288e3614b2d0674864e66b3919f62dd7db040e1
voon9128 <voonshunzhi@gmail.com>
2018-08-14 16:11:25 +0800

    will_paginate in airbnb controller

--- app/controllers/airbnbs_controller.rb
+++ app/controllers/airbnbs_controller.rb
@@ -2,7 +2,7 @@ class AirbnbsController < ApplicationController
 
 	before_action :require_login, only: [:index]
 	def index
-		@listings = Listing.where(status:"approved").order("created_at DESC")
+		@listings = Listing.where(status:"approved").order("created_at DESC").paginate(:page => params[:page], :per_page => 12)
 	end
 	def show
 		@airbnb = Listing.find(params[:id])
